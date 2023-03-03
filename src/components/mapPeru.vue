6caccb3ba26099758c72749a970a8ea4f96b0ae5
Vinicius Santos <vini_feliperodrigues@hotmail.com>
2023-03-03 15:14:28 -0300

    Setar valores labels

--- src/components/mapPeru.vue
+++ src/components/mapPeru.vue
@@ -14,40 +14,36 @@ export default {
   },
   data(){
     return {
-      objStateColors: {
-        AMA: am5.color("#FECA19"),
-        ANC: am5.color("#E8385F"),
-        APU: am5.color("#FECA19"),
-        ARE: am5.color("#E8385F"),
-        AYA: am5.color("#FECA19"),
-        CAJ: am5.color("#E8385F"),
-        CAL: am5.color("#E8385F"),
-        CUS: am5.color("#FECA19"),
-        HUC: am5.color("#FECA19"),
-        HUV: am5.color("#E8385F"),
-        ICA: am5.color("#E8385F"),
-        JUN: am5.color("#FECA19"),
-        LAL: am5.color("#E8385F"),
-        LAM: am5.color("#E8385F"),
-        LIM: am5.color("#E8385F"),
-        LMA: am5.color("#E8385F"),
-        LOR: am5.color("#252351"),
-        MDD: am5.color("#252351"),
-        MOQ: am5.color("#E8385F"),
-        PAS: am5.color("#FECA19"),
-        PIU: am5.color("#E8385F"),
-        PUN: am5.color("#FECA19"),
-        SAM: am5.color("#252351"),
-        TAC: am5.color("#E8385F"),
-        TUM: am5.color("#E8385F"),
-        UCA: am5.color("#252351"),
-        LKT: am5.color("#FECA19"),
+      objStatesData: {
+        AMA: {color: am5.color("#FECA19"), lat: -3.602249, lon: -78.411071, noPrazo: 1, atencao: 2, atrasada: 3},
+        ANC: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 4, atencao: 5, atrasada: 6},
+        APU: {color: am5.color("#FECA19"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        ARE: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        AYA: {color: am5.color("#FECA19"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        CAJ: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        CAL: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        CUS: {color: am5.color("#FECA19"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        HUC: {color: am5.color("#FECA19"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        HUV: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        ICA: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        JUN: {color: am5.color("#FECA19"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        LAL: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        LAM: {color: am5.color("#E8385F"), lat: -6.077157, lon: -82.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        LIM: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        LMA: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        LOR: {color: am5.color("#252351"), lat: -2.877157, lon: -73.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        MDD: {color: am5.color("#252351"), lat: -10.877157, lon: -70.557286, noPrazo: 1, atencao: 2, atrasada: 3},
+        MOQ: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        PAS: {color: am5.color("#FECA19"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        PIU: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        PUN: {color: am5.color("#FECA19"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        SAM: {color: am5.color("#252351"), lat: -6.360896, lon: -76.868080, noPrazo: 4, atencao: 5, atrasada: 6},
+        TAC: {color: am5.color("#E8385F"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
+        TUM: {color: am5.color("#E8385F"), lat: -3.177157, lon: -83.257286, noPrazo: 1, atencao: 2, atrasada: 3},
+        UCA: {color: am5.color("#252351"), lat: -8.877157, lon: -73.557286, noPrazo: 1, atencao: 2, atrasada: 3},
+        LKT: {color: am5.color("#FECA19"), lat: -4.877157, lon: -83.057286, noPrazo: 1, atencao: 2, atrasada: 3},
       },
-      objAuxLabels: {
-        noPrazo: 2,
-        atencao: 4,
-        atrasada: 3
-      }
+      auxIdx: -1,
     }
   },
   mounted() {
@@ -91,35 +87,48 @@ export default {
       })
     );
     pointSeries.bullets.push(function() {
+      thisVue.auxIdx++;
       return am5.Bullet.new(root, {
         sprite: am5.Label.new(root, {
           html:   `<div style="display: flex; gap: 5px">
                     <div style="width: 30px; height: 30px; background-color: green; color: white; position: relative">
-                      <span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">${thisVue.objAuxLabels.noPrazo}</span>
+                      <span id="noPrazo_${Object.keys(thisVue.objStatesData)[thisVue.auxIdx]}" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)"></span>
                     </div>
                     <div style="width: 30px; height: 30px; background-color: yellow; color: black; position: relative">
-                      <span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">${thisVue.objAuxLabels.atencao}</span>
+                      <span id="atencao_${Object.keys(thisVue.objStatesData)[thisVue.auxIdx]}" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)"></span>
                     </div>
                     <div style="width: 30px; height: 30px; background-color: red; color: black; position: relative">
-                      <span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">${thisVue.objAuxLabels.atrasada}</span>
+                      <span id="atrasada_${Object.keys(thisVue.objStatesData)[thisVue.auxIdx]}" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)"></span>
                     </div>
                   </div>`
         })
       });
     });
 
-    pointSeries.pushDataItem({ latitude: -7.360896, longitude: -76.868080 });
-
     let peruData = [];
-    Object.keys(this.objStateColors).forEach(state => {
+    Object.keys(this.objStatesData).forEach(state => {
       peruData.push({
         id: `PE-${state}`,
         polygonSettings: {
-          fill: this.objStateColors[state]
+          fill: this.objStatesData[state].color
         }
       });
+
+      if(this.objStatesData[state].lat && this.objStatesData[state].lon){
+        pointSeries.pushDataItem({ latitude: this.objStatesData[state].lat, longitude: this.objStatesData[state].lon });
+      }
     });
     peruSeries.data.setAll(peruData);
+
+    setTimeout(() => {
+      Object.keys(this.objStatesData).forEach(state => {
+        if(this.objStatesData[state].lat && this.objStatesData[state].lon){
+          document.getElementById(`noPrazo_${state}`).textContent = this.objStatesData[state].noPrazo;
+          document.getElementById(`atencao_${state}`).textContent = this.objStatesData[state].atencao;
+          document.getElementById(`atrasada_${state}`).textContent = this.objStatesData[state].atrasada;
+        }
+      });
+    }, 500);
   }
 }
 </script>
